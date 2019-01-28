4a1abffb1be702b9fbcee67e0cebdb0be4cbf5e2
ElaineGuo626 <46880452+ElaineGuo626@users.noreply.github.com>
2019-01-28 12:17:14 +0800

    Add files via upload

--- p5jsTutorial1.2-master/sketch.js
+++ p5jsTutorial1.2-master/sketch.js
@@ -25,21 +25,20 @@ function draw()
 	rect(150,10,100,50);
 	rect(150,100,100,50);
 
-  //Left
-  fill(133,61,228);
-	stroke(255,255,102);
+  //New triangles - Right
+  fill(255,178,102);
+	stroke(153,255,204);
 	strokeWeight(6);
 
 	rect(500,10,100,50);
-	rect(10,100,100,50);
 
-  //Right
-  fill(133,61,228);
-	stroke(255,255,102);
+  //New triangles - Left
+  fill(153,255,204);
+	stroke(255,178,102);
 	strokeWeight(6);
 
-	rect(360,10,100,50);
-	rect(10,100,100,50);
+	rect(390,10,100,50);
+
   //Worked with Avelyn
   //Findings:As the strokeweight increases, the gap between the two rectangles gets smaller
 }
