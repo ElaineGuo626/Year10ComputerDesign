1d11fa3faa81421a4e0e9296360d1e254eeaccf4
ElaineGuo626 <46880452+ElaineGuo626@users.noreply.github.com>
2019-02-25 13:10:39 +0800

    Add files via upload

---
+++ Button Class/Howtoplay/sketch.js
@@ -0,0 +1,28 @@
+var b1;
+var b2;
+//add a new variable every time you add a button
+
+function setup()
+{
+	createCanvas(500,500);
+	//(x, y, length, height)
+	b1 = new Button(10,10,100,25);
+	b1.setText("Elaine");
+	b1.setTextOver("GO");
+	b1.setButtonOverFill(50,255,50);
+	b1.setTextPressed("GO");
+}
+
+function draw()
+{
+	background(255,255,255);
+
+//18: to show the button
+	b1.showButton();
+//have we pressed b1?
+	if (b1.getButtonState() == 1)
+	{
+		//remember to type http://
+		window.open("buttonList/buttonList.html","_self");
+	}
+}
